e1c7251760e76b50fd457167b176b08f607bbb70
Nicolas Valdez <nico_v99@hotmail.com>
2025-07-18 16:27:46 -0300

    testing

--- utils/form.ts
+++ utils/form.ts
@@ -1,13 +1,8 @@
-import { getPersonalByEmail } from "@/database/dbPersonal";
-import { IPersonal } from "@/interfaces/personal";
-import { UseFormReturn } from "react-hook-form";
-import { z } from "zod";
-import { NuevaCuentaFormSchemaRefined } from "./validations";
 
-export const setearNombre = async(email: string, form: UseFormReturn<z.infer<typeof NuevaCuentaFormSchemaRefined>>) => {
-    const persona: IPersonal = await getPersonalByEmail(email);
-    if(persona){
-      form.setValue('nombre', persona.nombre);
-      form.setValue('apellido', persona.apellido);
-    }
-  }
+// export const setearNombre = async(email: string, form: UseFormReturn<z.infer<typeof NuevaCuentaFormSchemaRefined>>) => {
+//     const persona: IPersonal = await getPersonalByEmail(email);
+//     if(persona){
+//       form.setValue('nombre', persona.nombre);
+//       form.setValue('apellido', persona.apellido);
+//     }
+//   }
